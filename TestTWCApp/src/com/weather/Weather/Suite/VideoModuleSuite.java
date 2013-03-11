5e7ad59af25c0f6bd7e78932e0a15d9a4848f544
Rahul Khanna <rkhanna@xpanxion.co.in>
2013-03-11 15:45:24 +0530

    First Commit

---
+++ TestTWCApp/src/com/weather/Weather/Suite/VideoModuleSuite.java
@@ -0,0 +1,18 @@
+package com.weather.Weather.Suite;
+
+import java.io.IOException;
+
+import com.weather.Weather.test.VideosModule;
+
+public class VideoModuleSuite extends VideosModule{
+	
+	public void testSuite() throws InterruptedException, IOException{
+		//launchVideosTab();
+		//clickonMustSee();
+		//checkVideoIsPlaying();
+		//checOrientationForVideoTab();
+		//searchAndAddLocation();
+		checkVideoTitle();
+		
+	}
+}
